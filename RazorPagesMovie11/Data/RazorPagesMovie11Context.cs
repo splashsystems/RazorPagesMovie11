6ed08e2b3693edcc322a4f3307b630a53f17a3fe
Scot <bscot.hagen@gmail.com>
2021-06-15 14:54:09 -0700

    Added Model, scaffolding, initial migration

---
+++ RazorPagesMovie11/Data/RazorPagesMovie11Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie11.Models;
+
+namespace RazorPagesMovie11.Data
+{
+    public class RazorPagesMovie11Context : DbContext
+    {
+        public RazorPagesMovie11Context (DbContextOptions<RazorPagesMovie11Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie11.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovie11/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie11.Data;
+using RazorPagesMovie11.Models;
+
+namespace RazorPagesMovie11.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie11.Data.RazorPagesMovie11Context _context;
+
+        public IndexModel(RazorPagesMovie11.Data.RazorPagesMovie11Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
