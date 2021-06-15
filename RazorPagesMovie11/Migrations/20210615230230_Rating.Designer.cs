7efea3d0516231bf48128073d04bd31c8fad3e09
Scot <bscot.hagen@gmail.com>
2021-06-15 16:04:13 -0700

    added Search and new field Rating

--- RazorPagesMovie11/Migrations/20210615230230_Rating.Designer.cs
+++ RazorPagesMovie11/Migrations/20210615230230_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie11.Data;
 
 namespace RazorPagesMovie11.Migrations
 {
     [DbContext(typeof(RazorPagesMovie11Context))]
-    partial class RazorPagesMovie11ContextModelSnapshot : ModelSnapshot
+    [Migration("20210615230230_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
